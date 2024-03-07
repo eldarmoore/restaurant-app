f3fb46f40b82bcc3edd4c6491ba44b3ff97b8aeb
Eldar <eldar.moore@gmail.com>
2024-03-07 14:19:13 +0200

    Implement error and success notifications for category creation, add category record into the database

--- resources/views/management/category.blade.php
+++ resources/views/management/category.blade.php
@@ -15,6 +15,12 @@
                 <i class="fas fa-align-justify"></i> Category
                 <a class="btn btn-success btn-sm float-right" href="/management/category/create"><i class="fas fa-plus"></i> Create Category</a>
                 <hr>
+                @if(session()->has('status'))
+                    <div class="alert alert-success">
+                        <button type="button" class="close" data-dismiss="alert">X</button>
+                        {{Session()->get('status')}}
+                    </div>
+                @endif
             </div>
         </div>
     </div>
