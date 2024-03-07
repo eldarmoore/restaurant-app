a444886f8d4316d85f57f3be9c684405b41f95f4
Eldar <eldar.moore@gmail.com>
2024-03-07 15:28:56 +0200

    Add category edit functionality

--- app/Http/Controllers/Management/CategoryController.php
+++ app/Http/Controllers/Management/CategoryController.php
@@ -16,7 +16,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        $categories = Category::paginate(10);
+        $categories = Category::paginate(5);
         return view('management.category')->with('categories', $categories);
     }
 
@@ -66,7 +66,8 @@ class CategoryController extends Controller
      */
     public function edit($id)
     {
-        //
+        $category = Category::find($id);
+        return view('management.editCategory')->with('category', $category);
     }
 
     /**
@@ -78,8 +79,13 @@ class CategoryController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
-    }
+        $request->validate(['name' => 'required|unique:categories|max:255']);
+        $category = Category::find($id);
+        $category->name = $request->name;
+        $category->save();
+
+        $request->session()->flash('status', $request->name. " is updated successfully.");
+        return redirect('/management/category');    }
 
     /**
      * Remove the specified resource from storage.
