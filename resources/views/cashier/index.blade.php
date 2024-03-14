3e789dc7e89e2bdec79c4fdc87de6ecb47d1dd90
Eldar <eldar.moore@gmail.com>
2024-03-14 19:39:15 +0200

    Toggle All Tables in the Cashier Page by Using Ajax and jQuery.

--- resources/views/cashier/index.blade.php
+++ resources/views/cashier/index.blade.php
@@ -12,10 +12,21 @@
     </div>
     <script>
         $(document).ready(function () {
+            // Make table-detail hidden
+            $('#table-detail').hide();
+
+            // show all tables when click on the button
             $("#btn-show-tables").click(function () {
-                $.get("/cashier/getTable", function (data) {
-                    $("#table-detail").html(data);
-                });
+                if ($('#table-detail').is(":hidden")) {
+                    $.get("/cashier/getTable", function (data) {
+                        $("#table-detail").html(data);
+                        $("#table-detail").slideDown('fast');
+                        $("#btn-show-tables").html('Hide Tables').removeClass('btn-primary').addClass('btn-danger');
+                    });
+                } else {
+                    $("#table-detail").slideUp('fast');
+                    $("#btn-show-tables").html('Vide All Tables').removeClass('btn-danger').addClass('btn-primary');
+                }
             });
         });
     </script>
