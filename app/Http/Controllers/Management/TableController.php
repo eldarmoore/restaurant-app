0648afdf4acf48283e89b652a299ec6c04a1e45e
Eldar <eldar.moore@gmail.com>
2024-03-14 14:41:27 +0200

    Update a Table

--- app/Http/Controllers/Management/TableController.php
+++ app/Http/Controllers/Management/TableController.php
@@ -70,7 +70,8 @@ class TableController extends Controller
      */
     public function edit($id)
     {
-        //
+        $table = Table::find($id);
+        return view('management.editTable')->with('table', $table);
     }
 
     /**
@@ -82,7 +83,12 @@ class TableController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $request->validate(['name' => 'required|unique:tables|max:255']);
+        $table = Table::find($id);
+        $table->name = $request->name;
+        $table->save();
+        $request->session()->flash('status', 'The table is updated to ' . $request->name. ' successfully');
+        return redirect('/management/table');
     }
 
     /**
